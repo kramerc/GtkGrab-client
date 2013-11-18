8aaf506053718840198d7e1ab087cca3f0381857
Pieter Kokx <pieter@kokx.nl>
2013-11-18 22:34:47 +0100

    Made GtkGrab a distutils distribution.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+from distutils.core import setup
+
+setup(name='GtkGrab-client',
+      version='0.3.0',
+      description='Automatic screenshot upload utility',
+      license='GNU GPLv3',
+      maintainer='Pieter Kokx',
+      maintainer_email='pieter@kokx.nl',
+      py_modules=['pyperclip'],
+      scripts=['GtkGrab','record-gif.sh'],
+      data_files=[('share/GtkGrab', ['config.cfg-sample-linux','config.cfg-sample-mac'])],
+      url='https://github.com/kokx/GtkGrab-client')
